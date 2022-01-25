8c8408a972514e33708a837730e59a25980e1e24
arjun <arjun@vinnovatelabz.com>
2022-01-25 11:00:16 +0530

    basic features added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routes';
 import { SideNavComponent } from './components/side-nav/side-nav.component';
 import { HomeComponent } from './home/home.component';
-declare const OT:any;
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
+import { environment } from 'src/environments/environment';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { RecordingsComponent } from './recordings/recordings.component';
+
 @NgModule({
   declarations: [
     SideNavComponent,
-    HomeComponent
+    HomeComponent,
+    AppComponent,
+    RecordingsComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    MatSidenavModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatIconModule,
+    MatCardModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
   ],
+  bootstrap: [AppComponent],
   providers: [],
-  bootstrap: []
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/recordings/recordings.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from '../services/firebase.service';
+
+@Component({
+  selector: 'app-recordings',
+  templateUrl: './recordings.component.html',
+  styleUrls: ['./recordings.component.scss'],
+})
+export class RecordingsComponent implements OnInit {
+  recordings: any;
+  constructor(private firebaseService: FirebaseService) {}
+
+  ngOnInit(): void {
+    this.firebaseService.getItems().subscribe((recordings: any) => {
+      this.recordings = recordings;
+    });
+  }
+}
